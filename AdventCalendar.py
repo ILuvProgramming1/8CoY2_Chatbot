5079e49fd58e61a77c8928a911a49a66759d66da
ILuvProgramming1 <Chouriez26@kellettschool.com>
2024-12-02 00:56:04 +0000

    Mon, Dec 2, 2024, 12:56 AM +00:00

--- AdventCalendar.py
+++ AdventCalendar.py
@@ -1,5 +1,7 @@
 list1 = []
 list2 = []
+distance = 0
+sumDistance = 0
 for i in range(len(list1)-1):
     for j in range(len(list1)-i-1):
         if list1[j] > list1[j+1]:
@@ -9,4 +11,6 @@ for i in range(len(list2)-1):
         if list2[j] > list2[j+1]:
             list2[j], list2[j+1] = list2[j+1], list2[j]
 for i in range(len(list1)):
-    
+    distance = abs(list1[i] - list2[i])
+    sumDistance += distance
+
