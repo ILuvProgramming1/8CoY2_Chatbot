4422bca9942f5295174b426eb94c83b5b0f43fdc
ILuvProgramming1 <Chouriez26@kellettschool.com>
2024-12-02 00:53:26 +0000

    Mon, Dec 2, 2024, 12:53 AM +00:00

--- AdventCalendar.py
+++ AdventCalendar.py
@@ -1,7 +1,12 @@
-list1 = [9,8,7,6,5,4,3,2,1]
+list1 = []
 list2 = []
-for i in range(len(list1 -1)):
-    for j in range(len(list1 - i - 1)):
-        if list1[j] > list1[i+1]:
-            data[i+1] = data[j]
-print(list1)
+for i in range(len(list1)-1):
+    for j in range(len(list1)-i-1):
+        if list1[j] > list1[j+1]:
+            list1[j], list1[j+1] = list1[j+1], list1[j]
+for i in range(len(list2)-1):
+    for j in range(len(list2)-i-1):
+        if list2[j] > list2[j+1]:
+            list2[j], list2[j+1] = list2[j+1], list2[j]
+for i in range(len(list1)):
+    
