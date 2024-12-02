53289eef8f65efe88c2f249c9e05aa4a8246c778
ILuvProgramming1 <Chouriez26@kellettschool.com>
2024-12-02 00:39:33 +0000

    Mon, Dec 2, 2024, 12:39 AM +00:00

--- AdventCalendar.py
+++ AdventCalendar.py
@@ -1,5 +1,6 @@
 list1 = []
 list2 = []
-for i in range(len(list1 - 2))
-    for j in range(len(list1 - i - 2)
-        if 
+for i in range(len(list1 - 1)):
+    for j in range(len(list1 - 1)):
+        if list1[j] > list1[i+1]:
+            
