aef18d3dc9bd65e9768679bb6fb5384364e5e078
ILuvProgramming1 <Chouriez26@kellettschool.com>
2025-05-09 03:33:01 +0000

    Fri, May 9, 2025, 3:33 AM +00:00

--- StateTransitionThing.py
+++ StateTransitionThing.py
@@ -11,12 +11,12 @@ diddy.penup()
 diddy.goto(-GRID_SIZE/2, GRID_SIZE/2)
 diddy.pendown()
 
-for _ in range(4):
+for i in range(4):
     diddy.forward(GRID_SIZE)
     diddy.right(90)
 
-for _ in range(2):
-    for _ in range(1, sub_divisions):
+for i in range(2):
+    for i in range(1, sub_divisions):
         diddy.forward(cell_size)
         diddy.right(90)
         diddy.forward(GRID_SIZE)
